350ecf73fde5476c018edb4b9cecd2c9b19c9f37
wonwoo <lww0407@naver.com>
2022-03-03 23:54:34 +0900

    algo

--- prac.py
+++ prac.py
@@ -1,24 +1,14 @@
-#돌게임3
-N = int(input())
-ans = ''
-dp = [False] * 1010
-def recur(cur, cnt):
-    global ans
-    
-    
-    if cur == N:
-        if cnt % 2 == 1:
-            ans = 'SK'
-            return
-        else:
-            ans = 'CY'
-            return
-    if cur + 1 > N or cur + 3 > N or cur + 4 > N:
-        return
-    recur(cur + 1, cnt + 1)
-    recur(cur + 3, cnt + 1)
-    recur(cur + 4, cnt + 1)
-
-recur(0, 0)
-print(ans)
-# cnt가 홀수이면 상근이 이김
+T = int(input())
+for tc in range(T):
+    N = int(input()) #N 시간중에  ai 번 잠듬. 최대 1000000
+    li = list(map(int, input().split()))
+    max_num = max(li)
+    new_li = []
+    for i in range(1, len(li)):
+        if li[i] + li[i-1] == max_num:
+            new_li.append(li[i] + li[i-1])
+        elif li[i] == max_num:
+            new_li.append(li[i])
+        elif li[i] + li[i-1] > max_num:
+            continue
+        
