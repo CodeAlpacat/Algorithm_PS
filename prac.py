517d4a264f7dacfa342ecdebad8f278f4b46b4c3
wonwoo <lww0407@naver.com>
2022-03-21 01:05:14 +0900

    WW_Add: DFS_DP Solved BFS_Failed

--- prac.py
+++ prac.py
@@ -1,38 +1,59 @@
 from collections import deque
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
+def in_range(x, y):
+    return 0 <= x < M and 0 <= y < N
 
-def bfs(v):
-    q = deque()
-    q.append(v)
-    visited[v] = True
-    min_node = A[v]
-    while q:
-        n = q.popleft()
-        min_node = min(min_node, A[n])
-        for i in graph[n]:
-            if visited[i]:
-                continue
-            q.append(i)
-            visited[i] = True
-    return min_node
+# def bfs():
+#     q = deque()
+#     q.append([0, 0])
+#     visited[0][0] = True
+#     d = 0
+#     while q:
+#         size = len(q)
+#         for _ in range(size):
+#             x, y = q.popleft()
+            
+#             if x == M-1 and y == N-1:
+#                 d += 1
 
-N, M, k = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-graph = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
-for i in range(M):
-    v, w = map(int, input().split())
-    graph[v].append(w)
-    graph[w].append(v)
+#             for i in range(4):
+#                 nx = x + dx[i]
+#                 ny = y + dy[i]
+#                 if not in_range(nx, ny):
+#                     continue
+                
+#                 if graph[x][y] <= graph[nx][ny]:
+#                     continue
+                
+
+#                 q.append([nx, ny])
+
+#     return d
+
+def dfs(x, y):
+    if visited[x][y] != None:
+        return visited[x][y]
+
+    d = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if not in_range(nx, ny):
+            continue
+        
+        if graph[x][y] <= graph[nx][ny]:
+            continue
+
+        d += dfs(nx, ny)
     
-ans = 0
-for i in range(1, N+1):
-    if visited[i]:
-        continue
-    else:
-        ans += bfs(i)
+    visited[x][y] = d
+    return visited[x][y]
 
-if ans > k:
-    print("Oh no")
-else:
-    print(ans) 
+M, N = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(M)]
+visited = [[None] * N for _ in range(M)]
+visited[M-1][N-1] = 1
+print(dfs(0, 0))
