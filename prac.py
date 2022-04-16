cfd7109fe072b2aaaa924e8977a0c9d1e179148c
wonwoo <lww0407@naver.com>
2022-04-16 23:25:51 +0900

    algo

--- prac.py
+++ prac.py
@@ -2,5 +2,20 @@ from collections import deque
 import sys
 # sys.stdin=open('sample_input.txt')
 # import collections
-# import heapq
+import heapq
+
+def get_dist(x, y):
+    pq = []
+    
+    dist[]
+
+
+while True:
+    N = int(input())
+
+    if not N:
+        break
+
+    mat = [list(map(int, input().split())) for _ in range(N)]
+    dist = [[0xffffff] * N for _ in range(N)]
 
