2d0f704297ac21495a2c36eba80da8d5514a8d86
wonwoo <lww0407@naver.com>
2022-04-16 23:50:42 +0900

    algo

--- baekjoon_algo/baekjoon4485.py
+++ baekjoon_algo/baekjoon4485.py
@@ -1,32 +1,28 @@
-from collections import deque
-import sys
-# sys.stdin=open('sample_input.txt')
-# import collections
 import heapq
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-
 def get_dist():
     pq = []
-    dist[0][0] = mat[0][0]
-    heapq.heappush(pq, [mat[0][0], 0, 0])
     
+    dist[0][0] = 0
+    heapq.heappush(pq, [0, 0, 0])
+
     while pq:
-        d, x, y = heapq.heappop(pq)
+        d, cur_x, cur_y = heapq.heappop(pq)
         
-        if dist[x][y] != d:
+        if dist[cur_x][cur_y] != d:
             continue
-        
+
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
             if 0 <= nx < N and 0 <= ny < N:
-                nd = dist[x][y] + mat[nx][ny]
+                nd = dist[cur_x][cur_y] + mat[nx][ny]
                 
                 if dist[nx][ny] > nd:
                     dist[nx][ny] = nd
-                    heapq.heappush(pq, [nd, nx, ny])
+                    heapq.heappush(pq, (nd, nx, ny))
 
 cnt = 0
 while True:
@@ -39,4 +35,4 @@ while True:
     mat = [list(map(int, input().split())) for _ in range(N)]
     dist = [[0xffffff] * N for _ in range(N)]
     get_dist()
-    print(f'Problem {cnt}: {dist[N-1][N-1]}')
+    print(f'Problem {cnt}: {mat[0][0]+dist[N-1][N-1]}')
