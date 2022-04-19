9363b19f56420132e8c9b2aacc001c6135186451
wonwoo <lww0407@naver.com>
2022-04-19 22:26:40 +0900

    algo

---
+++ baekjoon_algo/baekjoon1629.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+A, B, C = map(int, input().split())
+
+print(pow(A, B, C))
--- prac.py
+++ prac.py
@@ -4,70 +4,8 @@ import sys
 # import collections
 import heapq
 
+import sys
 input = sys.stdin.readline
-N, E = map(int, input().split())
-v = [[] for _ in range(N+1)]
+A, B, C = map(int, input().split())
 
-for i in range(E):
-    a, b, c = map(int, input().split())
-    v[a].append([b, c])
-    v[b].append([a, c])
-
-
-must_pass = list(map(int, input().split()))
-must_pass.sort()
-
-def get_dist(s , e):
-    dist = [0xffffff] * (N+1)
-    pq = []
-    
-    dist[s] = 0
-    heapq.heappush(pq, [0, s])
-    
-    while pq:
-        d, cur = heapq.heappop(pq)
-        
-        if cur == e:
-            return d
-
-        if d != dist[cur]:
-            continue
-        
-        for i in v[cur]:
-            nxt = i[0]
-            nd = i[1] + dist[cur]
-
-            if nd < dist[nxt]:
-                dist[nxt] = nd
-                heapq.heappush(pq, [nd, nxt])
-    return -1
-
-a = get_dist(1, must_pass[0])
-a1 = get_dist(1, must_pass[1])
-b = get_dist(must_pass[0], must_pass[1])
-c = get_dist(must_pass[1], N)
-c1 = get_dist(must_pass[0], N)
-
-if a + b + c < a1 + b + c1:
-    if a == -1 or b == -1 or c == -1:
-        if a1 == -1 or b == -1 or c1 == -1:
-            print(-1)
-        else:
-            print(a1+b+c1)
-    else:
-        print(a+b+c)
-
-elif a + b + c > a1 + b + c1:
-    if a1 == -1 or b == -1 or c1 == -1:
-        if a == -1 or b== -1 or c == -1:
-            print(-1)
-        else:
-            print(a+b+c)
-    else:
-        print(a1+b+c1)
-
-else:
-    if a1 == -1 or b == -1 or c == -1 or a == -1 or b== -1 or c1 == -1:
-        print(-1)
-    else:
-        print(a + b + c)
+print(pow(A, B, C))
