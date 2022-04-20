ab37c696c3c8a61b56853f1d9137bdae8715d4fa
wonwoo <lww0407@naver.com>
2022-04-20 21:56:37 +0900

    start!

--- prac.py
+++ prac.py
@@ -3,9 +3,3 @@ import sys
 # sys.stdin=open('sample_input.txt')
 # import collections
 import heapq
-
-import sys
-input = sys.stdin.readline
-A, B, C = map(int, input().split())
-
-print(pow(A, B, C))
