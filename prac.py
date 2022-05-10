ac0a3e9316a672574d37d7834684b7d7f3736488
wonwoo <lww0407@naver.com>
2022-05-10 23:38:06 +0900

    algo

--- prac.py
+++ prac.py
@@ -4,3 +4,23 @@ import sys
 # import collections
 import heapq
 
+N = int(input())
+mat = list(map(int, input().split()))
+
+def recur(cur, cnt):
+    global ans
+    if cur > N:
+        return
+    
+    if cur == N:
+        ans = max(ans, cnt)
+        return
+    
+
+    if mat[cur] < 0:
+        recur(cur + 1, cnt)
+    recur(cur + 1, cnt + mat[cur])
+
+ans = -0xffffff
+recur(0, 0)
+print(ans)
