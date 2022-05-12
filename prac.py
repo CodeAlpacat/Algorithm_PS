b13200898209cec3d4e580f63f5d61c0768d0321
wonwoo <lww0407@naver.com>
2022-05-12 23:02:02 +0900

    algo

--- prac.py
+++ prac.py
@@ -4,3 +4,28 @@ import sys
 # import collections
 import heapq
 
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+mat = [list(input().split()) for _ in range (5)]
+res = []
+def dfs(x, y, cnt):
+    cnt += mat[x][y]
+    
+    if len(cnt) == 6:
+        if cnt not in res:
+            res.append(cnt)
+        return
+        
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            dfs(nx, ny, cnt)
+
+for i in range(5):
+    for j in range(5):
+        dfs(i, j, '')
+
+print(len(res))
