fe454bc550b4d048291caccbce9ba5f329598942
wonwoo <lww0407@naver.com>
2022-05-17 21:33:17 +0900

    algo explanation

--- prac.py
+++ prac.py
@@ -6,3 +6,19 @@ import heapq
 
 
 
+T = int(input()) #T는 케이스 개수 3
+
+
+for tc in range(1, T+1): #테스트 케이스 for 문 3번
+    input_list = list(map(int, input().split())) #인풋 받아서 리스트로 만들어줌
+    
+    answer = 0
+    #평균 값구하는 수만큼 반복
+    for i in range(len(input_list)):
+        answer += input_list[i]
+    
+    answer /= len(input_list) #float 자료형
+    if answer == int(answer): #소수점이 존재하면 int로 바꾼값하고 다르니까 그
+        print(f'#{tc} {int(answer)}')
+    else:
+        print(f'#{tc} {int(answer)+1}')
