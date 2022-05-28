d86f2beea9c3c37057b20b1de9e8155e184bc80d
wonwoo <lww0407@naver.com>
2022-05-29 00:18:18 +0900

    algo

--- SWEA_algo/1954_달팽이 숫자.py
+++ SWEA_algo/1954_달팽이 숫자.py
@@ -38,4 +38,43 @@ for i in range(len(list_result)):
     print(f'#{i+1}')
     for j in list_result[i]:
         print(*j)
+        
+
+##################################
+
+
+dx = [0, 1, 0, -1] # 우 하 좌 상
+dy = [1, 0, -1, 0]
+
+def snail():
+    x = 0
+    y = 0
+    direction = 0
+    for i in range(N*N):
+        snail_arr[x][y] = i + 1 # 1부터 시작해야함
+        #좌표를 이동하기
+        x += dx[direction]
+        y += dy[direction]
+        #이동한 곳이 숫자를 넣을 수 있는 곳일까?
+        # if 0 > x or x >= N or 0 > y or y >= N or snail_arr[x][y]:
+        if 0 <= x < N and 0 <= y < N and not snail_arr[x][y]: #
+            continue
+        else:
+            x -= dx[direction]
+            y -= dy[direction]
+
+            direction = (direction + 1) % 4 # 우 하 좌 상 1 2 3 0 1 2 3 3
+                                            # 0  1  2  3  방향은 4번인덱스가 없음
+            x += dx[direction]
+            y += dy[direction]
+
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    snail_arr = [[0] * N for _ in range(N)]
+    snail()
     
+    print(f'#{tc+1}')
+    for i in snail_arr:
+        print(*i)
--- SWEA_algo/1974-스도쿠 검증.py
+++ SWEA_algo/1974-스도쿠 검증.py
@@ -1,9 +1,3 @@
-from collections import deque
-import sys
-# sys.stdin=open('sample_input.txt')
-# import collections
-import heapq
-
 #가로 검증 / 몇 번째 줄인지 확인
 def check_hori(x):
     list_number = []
@@ -27,11 +21,11 @@ def check_vert(y):
         return False
 
 #현재 범위에 속한 3*3 박스를 검증한다.
-def check_box(x, y): # x, y를 입력받는다
-    x -= (x % 3) #반복의 시작 x 좌표
-    y -= (y % 3) #반복이 시작하는 y 좌표
-    list_number = [] #검증할 리스트
-    for i in range(x, x+3): #시작 좌표에서 시작좌표 + 2까지 검증해야한다. 0 1 2
+def check_box(x, y):
+    x -= (x % 3)
+    y -= (y % 3)
+    list_number = []
+    for i in range(x, x+3):
         for j in range(y, y+3):
             list_number.append(sudoku_map[i][j])
     
