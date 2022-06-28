6a5fbc5d6f1b79823d347de8552cbbbe050566bb
wonwoo <lww0407@naver.com>
2022-06-29 02:28:04 +0900

    Commit Algo

--- baekjoon_algo/baekjoon3273.py
+++ baekjoon_algo/baekjoon3273.py
@@ -1,12 +1,3 @@
-# sys.stdin=open('sample_input.txt')
-# import collections
-import heapq
-import math
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-
 n = int(input())
 mat = list(map(int, input().split()))
 x = int(input())
