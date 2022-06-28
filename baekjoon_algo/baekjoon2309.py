eedced795c7ce2e76b6e79992269c3665beca347
wonwoo <lww0407@naver.com>
2022-06-29 01:04:25 +0900

    Algo

--- baekjoon_algo/baekjoon2309.py
+++ baekjoon_algo/baekjoon2309.py
@@ -30,3 +30,30 @@ recur(0, 0)
 
 for i in ans:
     print(i)
+
+##################################
+
+mat = []
+total_spy = -100
+for i in range(9):
+    N = int(input())
+    mat.append(N)
+    total_spy += N
+
+mat.sort()
+s = 0
+e = 8
+
+while s < e:
+    
+    if mat[s] + mat[e] < total_spy:
+        s += 1
+    elif mat[s] + mat[e] > total_spy:
+        e -= 1
+    else:
+        break
+
+for i in range(len(mat)):
+    if i == s or i == e:
+        continue
+    print(mat[i])
--- prac.py
+++ prac.py
@@ -7,25 +7,29 @@ from collections import deque
 
 input = sys.stdin.readline
 
-#1 2 3 4 5 6 7
-#N = 100
-#2 * 50 = 100
-#4 * 25 = 100
-#5 * 20 = 100
+mat = []
+total_spy = -100
+for i in range(9):
+    N = int(input())
+    mat.append(N)
+    total_spy += N
 
-N = int(input())
-cnt = 0
-for i in range(2, N+1):
-    if i * i > N:
+mat.sort()
+s = 0
+e = 8
+
+while s < e:
+    
+    if mat[s] + mat[e] < total_spy:
+        s += 1
+    elif mat[s] + mat[e] > total_spy:
+        e -= 1
+    else:
         break
 
-    if N % i == 0:
-        cnt += 1
+for i in range(len(mat)):
+    if i == s or i == e:
+        continue
+    print(mat[i])
 
-if N == 1:
-    print("소수가 아닙니다")
-elif cnt == 1:
-    print("소수")
-else:
-    print('소수 아님')
-
+    
