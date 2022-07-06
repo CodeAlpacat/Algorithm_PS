81b247edd4a89f64e2a310bdd62fc59a145d43eb
wonwoo <lww0407@naver.com>
2022-07-06 21:57:03 +0900

    algo

--- baekjoon_algo/baekjoon1644.py
+++ baekjoon_algo/baekjoon1644.py
@@ -40,3 +40,42 @@ recur(0, 0, False)
 
 print(ans)
 
+################ 정답
+
+
+N = int(input())
+
+prime_list = [True for _ in range(N+1)]
+prime_list[0], prime_list[1] = False, False
+
+for i in range(2, len(prime_list)):
+    
+    for j in range(i*i, N+1, i):
+        if prime_list[j]:
+            prime_list[j] = False
+
+prime_mat = []
+for i in range(len(prime_list)):
+    if prime_list[i]:
+        prime_mat.append(i)
+
+prime_mat = prime_mat + [0]
+s = 0
+e = 0
+total = prime_mat[0]
+cnt = 0
+while e < len(prime_mat)-1:
+    
+    if total < N:
+        e += 1
+        total += prime_mat[e]
+    elif total > N:
+        total -= prime_mat[s]
+        s += 1
+    else:
+        cnt += 1
+        e += 1
+        total += prime_mat[e]
+
+print(*prime_mat)
+print(cnt)
--- prac.py
+++ prac.py
@@ -2,50 +2,32 @@
 # import collections
 import heapq
 import math
-from platform import node
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
-#에라토스테네스로 거른다
-#거른 소수들의 조합이 N이 되는 경우를 구한다.
+#시작 좌표나 Sii만 아니면 비교한 뒤 절대 값 비교
+#i < j 여야함
+
 
 N = int(input())
-
-prime_list = [True for _ in range(N+1)]
-prime_list[0], prime_list[1] = False, False
-
-for i in range(2, len(prime_list)):
-    
-    for j in range(i*i, N+1, i):
-        if prime_list[j]:
-            prime_list[j] = False
-
-prime_mat = []
-for i in range(len(prime_list)):
-    if prime_list[i]:
-        prime_mat.append(i)
-
-prime_mat = prime_mat + [0]
-s = 0
-e = 0
-total = prime_mat[0]
-cnt = 0
-while e < len(prime_mat)-1:
-    
-    if total < N:
-        e += 1
-        total += prime_mat[e]
-    elif total > N:
-        total -= prime_mat[s]
-        s += 1
-    else:
-        cnt += 1
-        e += 1
-        total += prime_mat[e]
-
-print(*prime_mat)
-print(cnt)
+mat = [list(map(int, input().split())) for _ in range(N)]
+ans = 0xffffff
 
 
+
+start_link = []
+for i in range(N):
+    for j in range(i, N):
+        if i != j:
+            start_link.append(mat[i][j] + mat[j][i])
+
+for i in range(len(start_link)):
+    for j in range(i, len(start_link)):
+        if i < j:
+            ans = min(ans, abs(start_link[j] - start_link[i]))
+        
+print(ans)
+print(start_link)
+#5, 9, 6, 6, 10, 7
