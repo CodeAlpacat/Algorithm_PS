80c6fd19ce8584fa51ace7cc862f1382a7644756
wonwoo <lww0407@naver.com>
2022-07-07 22:20:45 +0900

    Algo

--- baekjoon_algo/baekjoon1251.py
+++ baekjoon_algo/baekjoon1251.py
@@ -1,15 +1,3 @@
-# sys.stdin=open('sample_input.txt')
-# import collections
-import heapq
-import math
-import sys
-from collections import deque
-
-#mobitel
-#아스키 코드로 변환 => 정렬
-
-#mob ite l 정렬
-
 alpha = list(input())
 ans = []
 for i in range(1, len(alpha) - 1):
@@ -24,4 +12,4 @@ for i in range(1, len(alpha) - 1):
 for i in range(len(ans)):
     ans[i] = ''.join(ans[i])
 
-print(sorted(ans)[0])
+print(sorted(ans))
