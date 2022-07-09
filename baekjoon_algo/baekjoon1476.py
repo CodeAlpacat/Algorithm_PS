979ee16d0c52fc62e9eb095df27f2b0758ac8739
wonwoo <lww0407@naver.com>
2022-07-10 03:21:43 +0900

    algo

---
+++ baekjoon_algo/baekjoon1476.py
@@ -0,0 +1,28 @@
+import heapq
+import math
+import sys
+from collections import deque
+
+#지구 태양 달
+#E S M
+#15 28 19
+
+E, S, M = map(int, input().split())
+A, B, C = 1, 1, 1
+
+cnt = 1
+
+while E != A or S != B or M != C:
+    A += 1
+    B += 1
+    C += 1
+    
+    if A == 16:
+        A = 1
+    if B == 29:
+        B = 1
+    if C == 20:
+        C = 1
+    cnt += 1
+
+print(cnt)
