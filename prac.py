93d847887793ed33c9b9d9c832f674fd8f1c61f0
wonwoo <lww0407@naver.com>
2022-07-12 00:36:52 +0900

    algo

--- prac.py
+++ prac.py
@@ -7,17 +7,38 @@ from collections import deque
 
 # input = sys.stdin.readline
 
-N, M = map(int, input().split())
+#-5 -4 -3 -2 -1 0 1 2 3 4 5
+#5 4
+#1 ~ 9 중심 사이의 거리가 r1 + r2보다
 
-N_set = set()
-M_set = set()
+N = int(input())
+
+mat = []
 for i in range(N):
-    N_set.add(input())
-
-for i in range(M):
-    M_set.add(input())
-
-a = sorted(list(N_set.intersection(M_set)))
-print(len(a))
-for i in a:
-    print(i)
+    mat.append(list(map(int, input().split())))
+    
+def check():
+    for i in range(N):
+        for j in range(i, N):
+            if i == j:
+                continue
+            
+            if abs(mat[i][0] - mat[j][0]) <= mat[i][1] + mat[j][1]:
+                if mat[i][0]-mat[i][1] >
+                return 'NO'
+            
+            if mat[i][1] > mat[j][1]:
+                if mat[i][0] + mat[i][1] > mat[j][0] + mat[j][1] and mat[i][0] - mat[i][1] < mat[j][0] - mat[j][1]:
+                    pass
+                else:
+                    return 'NO'
+            elif mat[i][1] < mat[j][1]:
+                if mat[j][0] + mat[j][1] > mat[i][0] + mat[i][1] and mat[j][0] - mat[j][1] < mat[i][0] - mat[i][1]:
+                    pass
+                else:
+                    return 'NO'
+            else:
+                return 'NO'
+    else:
+        return 'YES'
+print(check())
