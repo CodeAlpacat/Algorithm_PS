f8c2b24f753ffd621a203b4107bec869f98f9794
wonwoo <lww0407@naver.com>
2022-07-13 22:06:35 +0900

    algo

--- baekjoon_algo/baekjoon14425.py
+++ baekjoon_algo/baekjoon14425.py
@@ -1,12 +1,3 @@
-# sys.stdin=open('sample_input.txt')
-# import collections
-import heapq
-import math
-import sys
-from collections import deque
-
-# input = sys.stdin.readline
-
 N, M = map(int, input().split())
 S = set()
 for i in range(N):
