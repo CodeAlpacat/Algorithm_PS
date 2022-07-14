43ce1c2f1bf4818846ea388ec8854fc2a0cf66da
wonwoo <lww0407@naver.com>
2022-07-14 22:32:36 +0900

    ALGO

--- prac.py
+++ prac.py
@@ -7,14 +7,3 @@ from collections import deque
 
 # input = sys.stdin.readline
 
-N, M = map(int, input().split())
-S = set()
-for i in range(N):
-    S.add(input())
-
-set_M = []
-cnt = 0
-for i in range(M):
-    if input() in S:
-        cnt += 1
-print(cnt)
