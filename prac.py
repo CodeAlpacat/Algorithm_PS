8642347f416ab042cd9f22297a7ab005d1350df1
wonwoo <lww0407@naver.com>
2022-07-25 23:40:30 +0900

    algo

--- prac.py
+++ prac.py
@@ -6,17 +6,4 @@ import math
 from collections import deque
 # input = sys.stdin.readline
 
-carved_str = input()
 
-N = int(input())
-
-cnt = 0
-for tc in range(N):
-    slot = list(input())
-    for i in range(10):
-        if ''.join(slot[:len(carved_str)]) == carved_str:
-            cnt += 1
-            break
-        slot.append(slot.pop(0))
-           
-print(cnt)
