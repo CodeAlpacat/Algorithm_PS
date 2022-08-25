8abc1d8262fa5ce4bae63366036b2bd28f31f6a5
wonwoo <lww0407@naver.com>
2022-08-26 01:31:18 +0900

    algo

--- prac.py
+++ prac.py
@@ -6,21 +6,15 @@ import math
 from collections import deque
 # input = sys.stdin.readline
 
-def recur(cur, cnt):
 
-    if cur == n:
-        if cnt == m:
-            return 1
-        else:
-            return 0
-        
-    if memo[cur][cnt] != -1:
-        return memo[cur][cnt]
+a, b = input().split()
 
-    memo[cur][cnt] = recur(cur + 1, cnt + 1) + recur(cur + 1, cnt)
-    return memo[cur][cnt]
+result = []
+for i in range(len(b) - len(a) + 1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            cnt += 1
+    result.append(cnt)
 
-n, m = map(int, input().split())
-memo = [[-1] * (n+1) for _ in range(n+1)]
-
-print(recur(0, 0))
+print(min(result))
