a501bb5046ddbd7140c53a0887cfa71fa89d50e5
wonwoo <lww0407@naver.com>
2022-10-11 02:23:49 +0900

    Algo

--- prac.py
+++ prac.py
@@ -4,12 +4,21 @@ import heapq
 import math
 # import sys
 
-while True:
-    n, m = map(int, input().split())
+X, Y = map(int, input().split())
+Z = (Y * 100) // X
+cnt = 0
+l = 1
+r = X
+#처음부터 100이 아니면 아무리 높아도 99
+if Z == 100 or Z == 99:
+    print(-1)
+else:
+    while l <= r:
+        m = (l + r) // 2
+        if (Y + m) * 100  // (X + m) <= Z:
+            l = m + 1
+        else:
+            cnt = m
+            r = m - 1
 
-    if n == 0 and m == 0:
-        break
-    if n == m + 1:
-        print("There is one tree.")
-    for i in range(n):
-        a, b = map(int, input().split())
+    print(cnt)
