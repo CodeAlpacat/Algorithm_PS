f77339aa1c4a0ba2132fddceba126924d629ed90
CodeAlpacat <magicmars123@gmail.com>
2022-10-12 01:48:29 +0900

    Algo

--- baekjoon_algo/baekjoon16562.py
+++ baekjoon_algo/baekjoon16562.py
@@ -35,3 +35,51 @@ if ans > k:
     print("Oh no")
 else:
     print(ans) 
+
+
+
+def find_(x):
+    if par[x] == x:
+        return x
+    else:
+        par[x] = find_(par[x])
+        return par[x]
+
+def union_(a, b):
+    a = find_(a)
+    b = find_(b)
+    
+    if a == b:
+        return
+    
+    par[a] = b
+    mat[b] = min(mat[a], mat[b])
+
+
+n, m, k = map(int, input().split())
+mat = [0] + list(map(int, input().split()))
+par = [i for i in range(n+1)]
+rnk = [0 for i in range(n+1)]
+total = 0
+for i in range(m):
+    a, b = map(int, input().split())
+    
+    #친구들끼리는 같은 트리 내에 존재
+    #가장 부모는 최소 비용만 가지고 있어야함
+    union_(a, b)
+
+visited = [0 for _ in range(n+1)]
+for i in range(1, n+1):
+    x = find_(i)
+
+    if visited[x]:
+        continue
+    
+    total += mat[x]
+    visited[x] = 1
+    
+if total > k:
+    print("Oh no")
+else:
+    print(total)
+    
