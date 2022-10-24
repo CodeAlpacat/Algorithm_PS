a3d65dcda0954fd7b5952dce0e2381afa43ba2b9
wonwoo <lww0407@naver.com>
2022-10-24 23:48:43 +0900

    algo

--- prac.py
+++ prac.py
@@ -4,15 +4,3 @@ import heapq
 import math
 import sys
 input = sys.stdin.readline
-
-N, M = map(int, input().split())
-arr = [[0 for _ in range(N+1)]] + [[0] + list(map(int, input().split())) for _ in range(N)]
-prefix = [[0] * (N+1) for _ in range(N+1)]
-
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j]
-    
-for _ in range(M):
-    x1, y1, x2, y2 = map(int, input().split())
-    print(prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1])
