d087bc7a5ef494889701cecd42d594dee5c299d7
wonwoo <lww0407@naver.com>
2022-10-25 00:58:41 +0900

    Algo

--- programmers/두 큐 합 같게 만들기.py
+++ programmers/두 큐 합 같게 만들기.py
@@ -1,10 +1,3 @@
-# sys.stdin=open('sample_input.txt')
-# import collections
-import heapq
-import math
-import sys
-input = sys.stdin.readline
-
 from collections import deque 
 def solution(queue1, queue2):
     answer = 0
