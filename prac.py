c222afe340a1b4fd86413655ec9a1f9e4c4b222b
WONWOOLEE <lww0407@naver.com>
2022-11-22 10:19:24 +0900

    algo

--- prac.py
+++ prac.py
@@ -3,21 +3,11 @@
 import heapq
 import math
 import sys
+
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-woods = list(map(int, input().split()))
-s, e = 1, max(woods)
-
-while s <= e:
-    m = (s+e) // 2
-
-    a = 0
-    for i in woods:
-        if i >= m:
-            a += i - m
-    if a >= M:
-        s = m + 1
-    else:
-        e = m - 1
-print(e)
+b1, b2, b3 = map(int, input().split())
+for tc in range(5):
+    k1, k2 = map(int, input().split())
+    
+    
