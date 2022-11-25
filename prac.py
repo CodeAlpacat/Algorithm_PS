59764e792b5619708453b56e5ed56c942e131cff
wonwoo <lww0407@naver.com>
2022-11-25 23:25:21 +0900

    algo

--- prac.py
+++ prac.py
@@ -3,11 +3,50 @@
 import heapq
 import math
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-b1, b2, b3 = map(int, input().split())
-for tc in range(5):
-    k1, k2 = map(int, input().split())
+
+#R, C로부터 거리가 D 이하인 칸들에 교통 정체
+dx = [0, -1, 0, 1]
+dy = [-1, 0, 1, 0]
+
+def bfs(x, y):
+    visited = [[0] * N for _ in range(M)]
+    q = deque()
+    q.append([x, y])
+    visited[x][y] = 1
     
+    d = 0
+    while q:
+
+        for _ in range(len(q)):
+            a, b = q.popleft()
+
+            if a == N and b == M:
+                return d
+
+            for i in range(4):
+                nx = a + dx[i]
+                ny = b + dy[i]
+                
+                if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and not mat[nx][ny]:
+                    visited[nx][ny] = 1
+                    q.append([nx, ny])
+        d += 1
+
+def get_traffic(x, y, d):
+    
+
+N, M = map(int, input().split())
+K = int(input())
+mat = [[0] * N for _ in range(M)]
+
+min_dist = -0xffffff
+for i in range(K):
+    R, C, D = map(int, input().split())
+    
+
+min_dist = min(min_dist, bfs(0, 0))
     
