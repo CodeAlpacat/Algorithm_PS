4d2710783c8cb983a577feb980000a868ded6366
wonwoo <lww0407@naver.com>
2022-12-08 04:04:57 +0900

    algo

--- prac.py
+++ prac.py
@@ -4,5 +4,17 @@ import math
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+# input = sys.stdin.readline
 
+n = input()
+
+n_len = len(n) - 1
+
+answer = 0
+
+for i in range(n_len):
+    answer += 9 * (10 ** i) * (i + 1)
+
+answer += ((int(n) - (10 ** n_len)) + 1) * (n_len + 1)
+
+print(answer)
