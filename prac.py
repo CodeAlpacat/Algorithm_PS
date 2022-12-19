2cc4363f2bf9ff7bca6b0c171756564dd6565582
wonwoo <lww0407@naver.com>
2022-12-19 10:35:47 +0900

    algo

--- prac.py
+++ prac.py
@@ -4,5 +4,19 @@ import math
 import sys
 from collections import deque
 
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
+N = int(input())
+mat = [list(map(int, input().split())) for _ in range(N)]
+mat.sort(key=lambda x: x[1])
+total = 0
+min_time = 0xffffff
+for i in range(N):
+    total += mat[i][0]
+    min_time = min(min_time, mat[i][1]-total)
+    if total > mat[i][1]:
+        print(-1)
+        exit()
+    
+
+print(min_time)
